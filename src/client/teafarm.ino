ac935ad8cd0a2f003a6d705d9b35b1e1ad1b43b6
ondralukes <40244594+ondralukes@users.noreply.github.com>
2019-01-31 10:59:37 +0100

    Update teafarm.ino

--- src/client/teafarm.ino
+++ src/client/teafarm.ino
@@ -245,24 +245,24 @@ bool cmddonereq(){
     return true;
   }
 void rotacePoSmeru() {
-  krok1();
-  krok2();
-  krok3();
-  krok4();
-  krok5();
-  krok6();
-  krok7();
-  krok8();
+  step1();
+  step2();
+  step3();
+  step4();
+  step5();
+  step6();
+  step7();
+  step8();
 }
 void rotaceProtiSmeru() {
-  krok8();
-  krok7();
-  krok6();
-  krok5();
-  krok4();
-  krok3();
-  krok2();
-  krok1();
+  step8();
+  step7();
+  step6();
+  step5();
+  step4();
+  step3();
+  step2();
+  step1();
 }
 void moff(){
    digitalWrite(in1, LOW);
@@ -270,56 +270,56 @@ void moff(){
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
   }
-void krok1(){
+void step1(){
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
   delayMicroseconds(speed);
 }
-void krok2(){
+void step2(){
   digitalWrite(in1, HIGH);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
   delayMicroseconds(speed);
 }
-void krok3(){
+void step3(){
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
   delayMicroseconds(speed);
 }
-void krok4(){
+void step4(){
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   delayMicroseconds(speed);
 }
-void krok5(){
+void step5(){
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   delayMicroseconds(speed);
 }
-void krok6(){
+void step6(){
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, HIGH);
   delayMicroseconds(speed);
 }
-void krok7(){
+void step7(){
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
   delayMicroseconds(speed);
 }
-void krok8(){
+void step8(){
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
